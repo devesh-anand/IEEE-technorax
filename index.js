c2d14d951b5504191c5d2535df1185080b8c5f7f
ishanailwal <isha.nailwal@gmail.com>
2021-08-30 20:50:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function thacks(){
+    document.getElementById("all-events-content").style.display="none";
+    document.getElementById("thacks-content").style.display="flex";
+    document.getElementById("rec-content").style.display="none";
+}
+
+function recno(){
+  document.getElementById("all-events-content").style.display="none";
+  document.getElementById("thacks-content").style.display="none";
+  document.getElementById("rec-content").style.display="flex";
+}
+
+function allcontent(){
+  document.getElementById("all-events-content").style.display="flex";
+  document.getElementById("thacks-content").style.display="none";
+  document.getElementById("rec-content").style.display="none";
+}
